460adad99a7e76f8ce3cb7073904026c9dd62892
Kristofer Espinosa <kristofer.espinosa@gmail.com>
2017-08-14 12:06:04 +0200

    rails good

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:query]
+    @answer = coach_answer(@question)
+  end
+
+  def coach_answer(your_message)
+    if your_message == "I am going to work right now!"
+      return ""
+    elsif your_message.include? "?"
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+end
